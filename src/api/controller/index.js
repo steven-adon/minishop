741bc596b90e01111ffe39614a98381740bdd505
luocooldong <gengyudonga@gmail.com>
2019-12-23 11:15:04 +0800

    配置

--- src/api/controller/index.js
+++ src/api/controller/index.js
@@ -4,16 +4,32 @@ module.exports = class extends Base {
   async indexAction() {
     const banner = await this.model('ad').where({ad_position_id: 1}).select();
     const channel = await this.model('channel').order({sort_order: 'asc'}).select();
+
+    // 获取最新商品
     const newGoods = await this.model('goods').field(['id', 'name', 'list_pic_url', 'retail_price']).where({is_new: 1}).limit(4).select();
+
+    console.log('❤️', newGoods)
+
+
     const hotGoods = await this.model('goods').field(['id', 'name', 'list_pic_url', 'retail_price', 'goods_brief']).where({is_hot: 1}).limit(3).select();
+
     const brandList = await this.model('brand').where({is_new: 1}).order({new_sort_order: 'asc'}).limit(4).select();
     const topicList = await this.model('topic').limit(3).select();
 
     const categoryList = await this.model('category').where({parent_id: 0, name: ['<>', '推荐']}).select();
+    console.log('❤️❤️', categoryList.length)
+
     const newCategoryList = [];
     for (const categoryItem of categoryList) {
+      console.log('❤️❤️Fuck', categoryItem.id)
       const childCategoryIds = await this.model('category').where({parent_id: categoryItem.id}).getField('id', 100);
-      const categoryGoods = await this.model('goods').field(['id', 'name', 'list_pic_url', 'retail_price']).where({category_id: ['IN', childCategoryIds]}).limit(7).select();
+
+
+      console.log('❤️❤️', childCategoryIds)
+
+      const categoryGoods = await this.model('goods').field(['id', 'name', 'list_pic_url', 'retail_price']).where({category_id: ['IN', childCategoryIds]}).limit(2).select();
+      console.log(categoryGoods)
+
       newCategoryList.push({
         id: categoryItem.id,
         name: categoryItem.name,
